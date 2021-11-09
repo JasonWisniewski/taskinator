9f63a1f7965d0aa1798a6d6928ad8421ba785f83
Jason Wisniewski <jwizinpa95@gmail.com>
2021-11-08 22:52:15 -0700

    js list items with drop down type

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,26 @@ var taskToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function (event){
 
+  // prevent page from reloading
   event.preventDefault();
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+  
+  // create list item 
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a  new task";
+
+  // create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  // give it a class name
+  taskInfoEl.className= "task-info"
+  // add html content to div
+  taskInfoEl.innerHTML = "<h3 calss='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  
+  listItemEl.appendChild(taskInfoEl);
+
+  // add entire list item to list
   taskToDoEl.appendChild(listItemEl);
 };
 
